e61a8d46af4890989d28d7361d0425a1c9e1178c
gabrielghost <charlie.gavins@neonseed.com>
2017-09-15 16:14:57 +0100

    dates importing and formatted correctly

--- src/utils/docGen.js
+++ src/utils/docGen.js
@@ -5,51 +5,89 @@ import path from 'path';
 import moment from 'moment';
 import _ from 'lodash';
 import { saveAs } from 'file-saver';
+import ntow from 'number-to-words';
 
-
+//this takes the title from the database and assigns a gender for the paragraph generation
+function titleGenderConversion(title, type){
+  if ((title = 'Mr') && (type != 'posses')){
+    return 'he'
+  }
+  if ((title = 'Mrs') && (type != 'posses')){
+    return 'she'
+  }
+  if ((title = 'Ms') && (type != 'posses')){
+    return 'she'
+  }
+  if ((title = 'Mr') && (type = 'posses')){
+    return 'his'
+  }
+  if ((title = 'Mrs') && (type = 'posses')){
+    return 'her'
+  }
+  if ((title = 'Ms') && (type = 'posses')){
+    return 'her'
+  }
+}
+//take all of these 'helper' functions into a separate JS file - including the date parser etc - will tidy every thing up 
+function capitalise(string){
+return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 export default function docGen(data, MoUInput){
-  console.log(data);
       var zip = new JSZip(MoUInput.data);
       var doc= new Docxtemplater().loadZip(zip)
       doc.setData({
-            OCCUPATION_A: data.OCCUPATION_A,
-            OCCUPATION_B: data.OCCUPATION_B,
-            LEGAL_ADVICE: data.LEGAL_ADVICE,
-            FIRST_NAME_A: data.FIRST_NAME_A,
-            LAST_NAME_A: data.LAST_NAME_A,
-            FIRST_NAME_B: data.FIRST_NAME_B,
-            LAST_NAME_B: data.LAST_NAME_B,
-            MEDIATOR_FIRST_NAME: data.MEDIATOR_FIRST_NAME,
-            NUMBER_OF_SESSIONS: data.NUMBER_OF_SESSIONS,
-            DATE_OF_MEDIATION_START: data.DATE_OF_MEDIATION_START,
-            DATE_OF_MEDIATION_END: data.DATE_OF_MEDIATION_END,
-            DATE_MARRIED: data.DATE_MARRIED,
-            DATE_COHABITED: data.DATE_COHABITED,
-            DATE_SEPARATED: data.DATE_SEPARATED,
-            PENSIONS_A: data.PENSIONS_A,
-            PENSIONS_B: data.PENSIONS_B,
-            OTHER_ASSETS_A: data.OTHER_ASSETS_A,
-            OTHER_ASSETS_B: data.OTHER_ASSETS_B,
-            BUSINESS_ASSETS_A: data.BUSINESS_ASSETS_A,
-            BUSINESS_ASSETS_B: data.BUSINESS_ASSETS_B,
-            LIABILITIES_A: data.LIABILITIES_A,
-            LIABILITIES_B: data.LIABILITIES_B,
-            PERSONAL_ASSETS_A: data.PERSONAL_ASSETS_A,
-            PERSONAL_ASSETS_B: data.PERSONAL_ASSETS_B,
-            OTHER_PROPERTY_A: data.OTHER_PROPERTY_A,
-            OTHER_PROPERTY_B: data.OTHER_PROPERTY_B,
-            FAMILY_HOME_A: data.FAMILY_HOME_A,
-            FAMILY_HOME_B: data.FAMILY_HOME_B,
-            DOB_A: data.DOB_A,
-            DOB_B: data.DOB_B,
-            NUMBER_OF_CHILDREN: data.NUMBER_OF_CHILDREN,
-            BACKGROUND_INFO_A: data.BACKGROUND_INFO_A,
-            BACKGROUND_INFO_B: data.BACKGROUND_INFO_B
+        case_number: data.case_number,
+        mediator_first_name: data.mediator_first_name,
+        mediator_last_name: data.mediator_last_name,
+        number_of_sessions: capitalise(ntow.toWords(data.number_of_sessions)),
+        date_of_mediation_start: data.date_of_mediation_start,
+        date_of_mediation_end: data.date_of_mediation_end,
+        legal_advice: data.legal_advice,
+        date_married: data.date_married,
+        date_cohabited: data.date_cohabited,
+        date_separated: data.date_separated,
+        title_a: data.title_a,
+        title_b: data.title_b,
+        first_name_a: data.first_name_a,
+        first_name_b: data.first_name_b,
+        last_name_a: data.last_name_a,
+        last_name_b: data.last_name_b,
+        dob_a: data.dob_a,
+        dob_b: data.dob_b,
+        occupation_a: data.occupation_a,
+        occupation_b: data.occupation_b,
+        new_partner_a: data.new_partner_a,
+        new_partner_b: data.new_partner_b,
+        new_partner_cohabiting_a: data.new_partner_cohabiting_a,
+        new_partner_cohabiting_b: data.new_partner_cohabiting_b,
+        new_partner_remarried_a: data.new_partner_remarried_a,
+        new_partner_remarried_b: data.new_partner_remarried_b,
+        new_partner_remarriage_intended_a: data.new_partner_remarriage_intended_a,
+        new_partner_remarriage_intended_b: data.new_partner_remarriage_intended_b,
+        good_health_a: data.good_health_a,
+        good_health_b: data.good_health_b,
+        good_health_description_a: data.good_health_description_a,
+        good_health_description_b: data.good_health_description_b,
+        family_home_a: data.family_home_a,
+        family_home_b: data.family_home_b,
+        other_property_a: data.other_property_a,
+        other_property_b: data.other_property_b,
+        personal_assets_a: data.personal_assets_a,
+        personal_assets_b: data.personal_assets_b,
+        liabilities_a: data.liabilities_a,
+        liabilities_b: data.liabilities_b,
+        business_assets_a: data.business_assets_a,
+        business_assets_b: data.business_assets_b,
+        other_assets_a: data.other_assets_a,
+        other_assets_b: data.other_assets_b,
+        pensions_a: data.pensions_a,
+        pensions_b: data.pensions_b
       });
 
       try {
           // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+          console.log(doc);
           doc.render()
       }
       catch (error) {
@@ -68,194 +106,13 @@ export default function docGen(data, MoUInput){
           type:"blob",
           mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
       }) //Output the document using Data-URI
-      console.log(out);
-      saveAs(out,"output.docx");
-  };
+      function fileName(){
+        if (data.last_name_a == data.last_name_b){
+        return `${data.case_number} ${data.last_name_b} MOU.docx`
+      } else {
+        return `${data.case_number} ${data.last_name_a} ${data.last_name_b} MOU.docx`
+      }
 
-// export default function docGen(data){
-//       mouBackground = backgroundInfoGeneration(data);
-//       mouProcessed = dataFormatCheck(mouBackground);
-//       docgen(mouProcessed, res);
-// }
-//
-// //this takes the title from the database and assigns a gender for the paragraph generation
-// function titleGenderConversion(title, type){
-//   if ((title = 'Mr') && (type != 'posses')){
-//     return 'he'
-//   }
-//   if ((title = 'Mrs') && (type != 'posses')){
-//     return 'she'
-//   }
-//   if ((title = 'Ms') && (type != 'posses')){
-//     return 'she'
-//   }
-//   if ((title = 'Mr') && (type = 'posses')){
-//     return 'his'
-//   }
-//   if ((title = 'Mrs') && (type = 'posses')){
-//     return 'her'
-//   }
-//   if ((title = 'Ms') && (type = 'posses')){
-//     return 'her'
-//   }
-//
-// }
-//
-//
-//
-// function backgroundInfoGeneration(data){
-//   // if (data.NUMBER_OF_CHILDREN = 0 || data.NUMBER_OF_CHILDREN = NULL){
-//   //   data.NUMBER_OF_CHILDREN = 'no';
-//   // }
-//   // if (data.NUMBER_OF_CHILDREN > 0){
-//   //   data.CHILDREN_INFO =
-//   // }
-//   if (data.LEGAL_ADVICE == false){
-//     data.LEGAL_ADVICE = `We have both been informed of the importance of obtaining legal advice during mediation and have decided not to take legal advice before implementing these proposals`;
-//   };
-//   if (data.LEGAL_ADVICE == true){
-//     data.LEGAL_ADVICE = `We have both been informed of the importance of obtaining legal advice during mediation and have decided to take legal advice to assist in implementing these proposals`;
-//   };
-//   if (data.GOOD_HEALTH_A && data.GOOD_HEALTH_B == true){
-//     data.too_Grammar = `too`;
-//   };
-//   if (data.NEW_PARTNER_A == false ){
-//     data.NEW_PARTNER_A = `does not have a new partner`;
-//   };
-//   if (data.NEW_PARTNER_A == true){
-//     data.NEW_PARTNER_A = `has a new partner`;
-//   };
-//   if (data.NEW_PARTNER_B == false){
-//     data.NEW_PARTNER_B = `does not have a new partner`;
-//   };
-//   if (data.NEW_PARTNER_B == true){
-//     data.NEW_PARTNER_B = `has a new partner`;
-//   };
-//   if (data.GOOD_HEALTH_A = true){
-//     data.GOOD_HEALTH_A = `is in good health`;
-//   };
-//   if (data.GOOD_HEALTH_B = true){
-//     data.GOOD_HEALTH_B = `is in good health`;
-//   };
-//   if (data.NEW_PARTNER_COHABITING_A == true){
-//     data.NEW_PARTNER_COHABITING_A = `is cohabiting with the new partner`
-//   };
-//   if (data.NEW_PARTNER_COHABITING_A == false){
-//     data.NEW_PARTNER_COHABITING_A = `is not cohabiting with the new partner`
-//   };
-//   if (data.NEW_PARTNER_REMARRIED_A == true){
-//     data.NEW_PARTNER_REMARRIED_A = `and has subsequently married`
-//   };
-//   if (data.NEW_PARTNER_REMARRIAGE_INTENDED_A == true){
-//     data.BACKGROUND_INFO_FULL_STOP = '. ';
-//     data.NEW_PARTNER_REMARRIAGE_INTENDED_A = `and is intending to get married`
-//   };
-//   data.BACKGROUND_INFO_A = `${titleGenderConversion(data.TITLE_A)} ${data.GOOD_HEALTH_A} and ${data.NEW_PARTNER_A}`;
-//   data.BACKGROUND_INFO_B = `${titleGenderConversion(data.TITLE_B)} ${data.too_Grammar} ${data.GOOD_HEALTH_B} and ${data.NEW_PARTNER_B}`;
-//   return data
-// };
-//
-// function dateParser(date){
-//   return moment(date).format('Do MMMM YYYY');
-// };
-//
-// function capitalise(string){
-//   return string.charAt(0).toUpperCase() + string.slice(1);
-// };
-//
-// function dataFormatCheck(mou){
-//   for (var i in mou){
-//     let value = mou[i];
-//     if (value == undefined){
-//       mou[i] = 'X';
-//     }
-//     if (_.isString(value)){
-//       mou[i] = capitalise(value);
-//     }
-//     if (_.isDate(value)){
-//       mou[i] = dateParser(value);
-//     }
-//     if (_.isNumber(value)){
-//       mou[i] = _.round((value), 2);
-//     }
-//   };
-//   return mou;
-// };
-//
-// function docgen(data, res){
-//
-//   //Load the docx file as a binary
-//   let content = fs
-//   .readFileSync(path.resolve(__dirname, '../doc-templates/MOU_input.docx'), 'binary');
-//
-//   let zip = new JSZip(content);
-//
-//   let doc = new Docxtemplater();
-//   doc.loadZip(zip);
-//
-// //CAN WE ES6 this so it's just { LEGAL_ADVICE, FIRST_NAME_A etc?}
-//   doc.setData({
-//     OCCUPATION_A: data.OCCUPATION_A,
-//     OCCUPATION_B: data.OCCUPATION_B,
-//     LEGAL_ADVICE: data.LEGAL_ADVICE,
-//     FIRST_NAME_A: data.FIRST_NAME_A,
-//     LAST_NAME_A: data.LAST_NAME_A,
-//     FIRST_NAME_B: data.FIRST_NAME_B,
-//     LAST_NAME_B: data.LAST_NAME_B,
-//     MEDIATOR_FIRST_NAME: data.MEDIATOR_FIRST_NAME,
-//     NUMBER_OF_SESSIONS: data.NUMBER_OF_SESSIONS,
-//     DATE_OF_MEDIATION_START: data.DATE_OF_MEDIATION_START,
-//     DATE_OF_MEDIATION_END: data.DATE_OF_MEDIATION_END,
-//     DATE_MARRIED: data.DATE_MARRIED,
-//     DATE_COHABITED: data.DATE_COHABITED,
-//     DATE_SEPARATED: data.DATE_SEPARATED,
-//     PENSIONS_A: data.PENSIONS_A,
-//     PENSIONS_B: data.PENSIONS_B,
-//     OTHER_ASSETS_A: data.OTHER_ASSETS_A,
-//     OTHER_ASSETS_B: data.OTHER_ASSETS_B,
-//     BUSINESS_ASSETS_A: data.BUSINESS_ASSETS_A,
-//     BUSINESS_ASSETS_B: data.BUSINESS_ASSETS_B,
-//     LIABILITIES_A: data.LIABILITIES_A,
-//     LIABILITIES_B: data.LIABILITIES_B,
-//     PERSONAL_ASSETS_A: data.PERSONAL_ASSETS_A,
-//     PERSONAL_ASSETS_B: data.PERSONAL_ASSETS_B,
-//     OTHER_PROPERTY_A: data.OTHER_PROPERTY_A,
-//     OTHER_PROPERTY_B: data.OTHER_PROPERTY_B,
-//     FAMILY_HOME_A: data.FAMILY_HOME_A,
-//     FAMILY_HOME_B: data.FAMILY_HOME_B,
-//     DOB_A: data.DOB_A,
-//     DOB_B: data.DOB_B,
-//     NUMBER_OF_CHILDREN: data.NUMBER_OF_CHILDREN,
-//     BACKGROUND_INFO_A: data.BACKGROUND_INFO_A,
-//     BACKGROUND_INFO_B: data.BACKGROUND_INFO_B
-//
-//   });
-//
-//
-//   try {
-//     // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-//     doc.render()
-//   }
-//   catch (error) {
-//     let e = {
-//       message: error.message,
-//       name: error.name,
-//       stack: error.stack,
-//       properties: error.properties,
-//     }
-//     console.log(JSON.stringify({error: e}));
-//     // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
-//     return res.status(500).json({ message: 'Something went wrong.', error: JSON.stringify(error)  });
-//   }
-//
-//   let buf = doc.getZip()
-//   .generate({type: 'nodebuffer'});
-//
-//   // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-//   fs.writeFileSync(path.resolve(__dirname, '../docs/output.docx'), buf);
-//   return res.status(200).json({ message: 'Document spawned'});
-// };
-//
-// module.exports = {
-//   gen: gen
-// };
+      }
+      saveAs(out,fileName());
+  };
--- src/utils/docParse.js
+++ src/utils/docParse.js
@@ -2,86 +2,105 @@ import XLSX from 'xlsx';
 import path from 'path';
 import moment from 'moment';
 
+function capitalise(string){
+return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 export default function docParse(fileBinary){
   const workbook = XLSX.read(fileBinary, {type: 'binary'});
   const first_sheet_name = workbook.SheetNames[0];
   const worksheet = workbook.Sheets[first_sheet_name];
   const MoUInput = {
 
-    MEDIATOR_FIRST_NAME: getCellValue('B2', 2),
-    MEDIATOR_LAST_NAME: getCellValue('B3', 2),
+    mediator_first_name: getCellValue('B2', 5),
+    mediator_last_name: getCellValue('B3', 5),
 
 
-    NUMBER_OF_SESSIONS: getCellValue('B6', 1),
-    DATE_OF_MEDIATION_START: getCellValue('B7', 1, 'd'),
-    DATE_OF_MEDIATION_END: getCellValue('B8', 1, 'd'),
-    LEGAL_ADVICE: getCellValue('B9', 1),
-    DATE_MARRIED: getCellValue('B10', 1, 'd'),
-    DATE_COHABITED: getCellValue('B11', 1, 'd'),
-    DATE_SEPARATED: getCellValue('B12', 1, 'd'),
+    number_of_sessions: getCellValue('B6', 4),
+    date_of_mediation_start: getCellValue('B7', 4, 'dd'),
+    date_of_mediation_end: getCellValue('B8', 4, 'dd'),
+    legal_advice: getCellValue('B9', 4),
+    date_married: getCellValue('B10', 4, 'dd'),
+    date_cohabited: getCellValue('B11', 4, 'dm'),
+    date_separated: getCellValue('B12', 4, 'dm'),
+    case_number: getCellValue('B13', 4),
 
-    TITLE_A: getCellValue('B22', 1),
-    TITLE_B: getCellValue('E22', 1),
-    FIRST_NAME_A: getCellValue('B23', 1),
-    FIRST_NAME_B: getCellValue('E23', 1),
-    LAST_NAME_A: getCellValue('B24', 1),
-    LAST_NAME_B: getCellValue('E24', 1),
-    DOB_A: getCellValue('B25', 1, 'd'),
-    DOB_B: getCellValue('E25', 1, 'd'),
-    OCCUPATION_A: getCellValue('B26', 1),
-    OCCUPATION_B: getCellValue('E26', 1),
-    NEW_PARTNER_A: getCellValue('B27', 1),
-    NEW_PARTNER_B: getCellValue('E27', 1),
-    NEW_PARTNER_COHABITING_A: getCellValue('B28', 1),
-    NEW_PARTNER_COHABITING_B: getCellValue('E28', 1),
-    NEW_PARTNER_REMARRIED_A: getCellValue('B29', 1),
-    NEW_PARTNER_REMARRIED_B: getCellValue('E29', 1),
-    NEW_PARTNER_REMARRIAGE_INTENDED_A: getCellValue('B30', 1),
-    NEW_PARTNER_REMARRIAGE_INTENDED_B: getCellValue('E30', 1),
-    GOOD_HEALTH_A: getCellValue('B31', 1),
-    GOOD_HEALTH_B: getCellValue('E31', 1),
-    GOOD_HEALTH_DESCRIPTION_A: getCellValue('B32', 1),
-    GOOD_HEALTH_DESCRIPTION_B: getCellValue('E32', 1),
+    title_a: getCellValue('B22', 4),
+    title_b: getCellValue('E22', 4),
+    first_name_a: getCellValue('B23', 4),
+    first_name_b: getCellValue('E23', 4),
+    last_name_a: getCellValue('B24', 4),
+    last_name_b: getCellValue('E24', 4),
+    dob_a: getCellValue('B25', 4, 'dd'),
+    dob_b: getCellValue('E25', 4, 'dd'),
+    occupation_a: getCellValue('B26', 4),
+    occupation_b: getCellValue('E26', 4),
+    new_partner_a: getCellValue('B27', 4),
+    new_partner_b: getCellValue('E27', 4),
+    new_partner_cohabiting_a: getCellValue('B28', 4),
+    new_partner_cohabiting_b: getCellValue('E28', 4),
+    new_partner_remarried_a: getCellValue('B29', 4),
+    new_partner_remarried_b: getCellValue('E29', 4),
+    new_partner_remarriage_intended_a: getCellValue('B30', 4),
+    new_partner_remarriage_intended_b: getCellValue('E30', 4),
+    good_health_a: getCellValue('B31', 4),
+    good_health_b: getCellValue('E31', 4),
+    good_health_description_a: getCellValue('B32', 4),
+    good_health_description_b: getCellValue('E32', 4),
 
 
-    FAMILY_HOME_A: getCellValue('K130', 0),
-    FAMILY_HOME_B: getCellValue('M130', 0),
-    OTHER_PROPERTY_A: getCellValue('K131', 0),
-    OTHER_PROPERTY_B: getCellValue('M131', 0),
-    PERSONAL_ASSETS_A: getCellValue('K132', 0),
-    PERSONAL_ASSETS_B: getCellValue('M132', 0),
-    LIABILITIES_A: getCellValue('K133', 0),
-    LIABILITIES_B: getCellValue('M133', 0),
-    BUSINESS_ASSETS_A: getCellValue('K134', 0),
-    BUSINESS_ASSETS_B: getCellValue('M134', 0),
-    OTHER_ASSETS_A: getCellValue('K135', 0),
-    OTHER_ASSETS_B: getCellValue('M135', 0),
-    PENSIONS_A: getCellValue('K137', 0),
-    PENSIONS_B: getCellValue('M137', 0)
+    family_home_a: getCellValue('K130', 0),
+    family_home_b: getCellValue('M130', 0),
+    other_property_a: getCellValue('K131', 0),
+    other_property_b: getCellValue('M131', 0),
+    personal_assets_a: getCellValue('K132', 0),
+    personal_assets_b: getCellValue('M132', 0),
+    liabilities_a: getCellValue('K133', 0),
+    liabilities_b: getCellValue('M133', 0),
+    business_assets_a: getCellValue('K134', 0),
+    business_assets_b: getCellValue('M134', 0),
+    other_assets_a: getCellValue('K135', 0),
+    other_assets_b: getCellValue('M135', 0),
+    pensions_a: getCellValue('K137', 0),
+    pensions_b: getCellValue('M137', 0)
   }
 
   //pass in the cell coordinates, sheet number, and format (when it is a date) and return the cell value.
-function getCellValue(cellAddress, sheetNumber, cellFormat){
-  let output_value;
-  const first_sheet_name = workbook.SheetNames[sheetNumber];
-  const worksheet = workbook.Sheets[first_sheet_name];
-  const address_of_cell = cellAddress;
-  const desired_cell = worksheet[address_of_cell];
-  let desired_value = (desired_cell ? desired_cell.v : undefined);
-  if ((cellFormat == 'd') && (typeof desired_value == 'number')){
-    let unParsedDate = XLSX.SSF.parse_date_code(desired_value);
-    output_value = parseExcelDate(unParsedDate);
-  } else {
-    output_value = desired_value;
-  }
-  return output_value;
-}
+  function getCellValue(cellAddress, sheetNumber, dateFormat){
+    let output_value;
+    const worksheet = workbook.Sheets[workbook.SheetNames[sheetNumber]];
+    const desired_cell = worksheet[cellAddress];
+    let desired_value = (desired_cell ? desired_cell.v : undefined);
 
-function parseExcelDate(unParsedDate){
-  //ensure that day and month are double digits (e.g. 01 for january or the first of the month) which ensures that moment parses the date correctly
-  unParsedDate.d = ('0' + unParsedDate.d).slice(-2);
-  unParsedDate.m = ('0' + unParsedDate.m).slice(-2);
-  return moment(`${unParsedDate.y}${unParsedDate.m}${unParsedDate.d}`, "YYYY-MM-DD");
+    //if cell format is a date ('d' passed through as an argument) and a check whether it is a number, then take the excel date format, and parse it into a moment date.
+    if ((dateFormat == ('dm')) && (typeof desired_value == 'number') || (dateFormat == ('dd')) && (typeof desired_value == 'number')){
+      output_value = parseExcelDate(desired_value, dateFormat);
+    }
+    if (desired_value == undefined){
+      output_value = 'X';
+    }
+    if (_.isString(desired_value)){
+      output_value = capitalise(desired_value);
+      console.log(output_value);
+    }
+    if (_.isNumber(desired_value) && (!dateFormat)){
+      output_value = _.round((desired_value), 2);
+    }
+    if (_.isBoolean(desired_value)){
+      output_value = desired_value;
+    }
+    return output_value;
+  }
+
+//take date from excel code to XLSX parsed, to moment formatted (e.g. 1st January 2017)
+function parseExcelDate(unParsedDate, dateFormat){
+  if (dateFormat == 'dd'){
+    return moment(XLSX.SSF.parse_date_code(unParsedDate)).format('Do MMMM YYYY');
+  }
+  if (dateFormat == 'dm'){
+    return moment(XLSX.SSF.parse_date_code(unParsedDate)).format('MMMM YYYY');
+  }
+
 }
 
 return MoUInput;
